4e2031ae65ab2dbc5c5354b6703ddc9c2e92db9e
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2019-05-14 13:50:31 -0700

    Add overlapping symmetry mode

--- sym.py
+++ sym.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 
-division = False
+mode = 0
 
 cap = cv.VideoCapture(0)
 
@@ -25,19 +25,23 @@ while True:
 
     output = None
 
-    if division: 
-        output = np.zeros_like(frame)
-        output[0:height // 2, 0:width // 2, :] = frame[:height // 2, :width // 2, :]
-        output[height // 2:, 0:width // 2, :] = frame_flipped_y[height // 2:, :width // 2, :]
-        output[0:height // 2, width // 2:, :] = frame_flipped_x[:height // 2, width // 2:, :]
-        output[height // 2:, width // 2:, :] = frame_flipped_xy[height // 2:, width // 2:, :]
-    else:        
+    if mode == 0:
         output = np.zeros((height * 2, width * 2, channels), 'uint8')
         output[0:height, 0:width, :] = frame[:, :, :]
         output[height:height * 2, 0:width, :] = frame_flipped_y[:, :, :]
         output[0:height, width:width*2, :] = frame_flipped_x[:, :, :]
         output[height:height * 2, width:width*2, :] = frame_flipped_xy[:, :, :]
-        output = cv.resize(output, (width, height), interpolation = cv.INTER_AREA)
+        output = cv.resize(output, (width, height), interpolation = cv.INTER_AREA)        
+    elif mode == 1:        
+        output = np.zeros_like(frame)
+        output[0:height // 2, 0:width // 2, :] = frame[:height // 2, :width // 2, :]
+        output[height // 2:, 0:width // 2, :] = frame_flipped_y[height // 2:, :width // 2, :]
+        output[0:height // 2, width // 2:, :] = frame_flipped_x[:height // 2, width // 2:, :]
+        output[height // 2:, width // 2:, :] = frame_flipped_xy[height // 2:, width // 2:, :]
+    elif mode == 2:
+        c1 = cv.addWeighted(frame, 0.5, frame_flipped_y, 0.5,0)
+        c2 = cv.addWeighted(frame_flipped_x, 0.5, frame_flipped_xy, 0.5,0)
+        output = cv.addWeighted(c1, 0.5, c2, 0.5,0)
 
     # Display the resulting frame
     cv.imshow('frame', output)
@@ -45,7 +49,10 @@ while True:
     if wait == ord('q'):
         break
     elif wait == ord('d'):
-        division = not division
+        mode += 1
+
+        if mode >= 3:
+            mode = 0
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
